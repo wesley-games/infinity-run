839163b2c5115d9d03925ab9d036c72e8f88f010
Wesley Rocha <wesley.rocha@hexagonagriculture.com>
2019-03-06 13:40:20 -0300

    Adding background

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{   
+    public float speed = 10;
+    public float endPosition = -18.9f;
+    public float startPosition = 18.9f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        if (transform.position.x <= endPosition ) {
+            Vector2 position = new Vector2(startPosition, transform.position.y);
+            transform.position = position;
+        }
+    }
+}
---
+++ Assets/Scripts/SoundDestroyer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundDestroyer : MonoBehaviour
+{
+    public float timeToDestroy = 1f;
+
+    private float counter = 0f;
+
+    void Update()
+    {
+        if (counter > timeToDestroy) {
+            Destroy(gameObject);
+            counter = 0f;
+        } else {
+            counter += Time.deltaTime;
+        }
+    }
+}
